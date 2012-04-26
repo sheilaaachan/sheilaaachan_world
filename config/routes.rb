545bee6fa869578517ee18caef64e2930ddebd5b
Elina Kim <elina.yjkim@gmail.com>
2012-04-25 21:17:12 -0400

    done

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ KcwWorld::Application.routes.draw do
   resources :categories
   resources :proverbs
   resources :photos
+  resources :articles
     
   match 'home' => 'home#index', :as => :home
   root :to => 'home#index'
